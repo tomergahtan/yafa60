905a3a2a7783762353a639a168d14151cc70d427
tomergahtan <121878273+tomergahtan@users.noreply.github.com>
2025-02-04 13:54:30 +0200

    update

--- src/components/imagebless.component/imagebless.component.tsx
+++ src/components/imagebless.component/imagebless.component.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './imagebless.style.scss';
 const ImageBless = ({ imageNames }: { imageNames: string[] }) => {
 
-    console.log("imageNames:", imageNames);
+    
 
     
     return (
         <div className='imagebless-post'>
-            <h1>example text</h1>
+            {/* <h1>example text</h1> */}
             
             {imageNames.map((imageName, i) => {
                 const imgSrc = require(`../../media/images/${imageName}`); 
--- src/components/videobless.component/videobless.component.tsx
+++ src/components/videobless.component/videobless.component.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './videobless.style.scss';
 
-const VideoBless = ({ videoNames }: { videoNames: string }) => {
-    const videourl = require(`../../media/videos/${videoNames}`);
+const VideoBless = ({ videoNames }: { videoNames: string[] }) => {
+    const videourl = require(`../../media/videos/${videoNames[0]}`);
+    
     return (
         <div className="videobless-post">
-            <h1>Video Blessing</h1>
+            {/* <h1>Video Blessing</h1> */}
             
             <video className="videobless-video" controls>
                     <source src={videourl} type="video/mp4" />
